eb05011cbcbafd59b2a6173a4600fa4deee3d395
PugVal <valechka26091989@ut.by>
2017-12-13 14:02:20 +0300

    2. fix Jdbc tests

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,10 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.JpaUtil;
@@ -26,12 +29,18 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     private CacheManager cacheManager;
 
     @Autowired
-    protected JpaUtil jpaUtil;
+    private Environment environment;
+
+    //@Autowired
+    private JpaUtil jpaUtil;
 
     @Before
     public void setUp() throws Exception {
+       if (!environment.acceptsProfiles(Profiles.JDBC))
+       {
+           jpaUtil.clear2ndLevelHibernateCache();
+       }
         cacheManager.getCache("users").clear();
-        jpaUtil.clear2ndLevelHibernateCache();
     }
 
     @Test
@@ -92,6 +101,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Test
     public void testValidation() throws Exception {
+        Assume.assumeTrue(!environment.acceptsProfiles(Profiles.JDBC));
         validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
