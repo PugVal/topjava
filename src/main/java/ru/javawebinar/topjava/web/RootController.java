1d9facf7bb10baa664fa1bea0871b16de494baee
PugVal <valechka26091989@ut.by>
2017-12-14 15:50:19 +0300

    1.3. RootContrller (add delete + getAll meals)

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -3,17 +3,27 @@ package ru.javawebinar.topjava.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 public class RootController {
     @Autowired
     private UserService service;
+
+    @Autowired
+    private MealRestController mealController;
 
     @GetMapping("/")
     public String root() {
@@ -32,4 +42,17 @@ public class RootController {
         AuthorizedUser.setId(userId);
         return "redirect:meals";
     }
+
+    @GetMapping("/meals")
+    public String meals(Model model) {
+        model.addAttribute("meals", mealController.getAll());
+        return "meals";
+    }
+
+    @GetMapping("meals/delete/{id}")
+    public String deleteMeal(@PathVariable("id") int id){
+       mealController.delete(id);
+        return "redirect:/meals";
+    }
+
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -2,6 +2,8 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://topjava.javawebinar.ru/functions" %>
+
+
 <html>
 <head>
     <title>Calories management</title>
@@ -12,7 +14,7 @@
     <h3>Meals</h3>
     <form method="post" action="meals?action=filter">
         <dl>
-            <dt>From Date:</dt>
+                <dt>From Date:</dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
         </dl>
         <dl>
@@ -54,7 +56,7 @@
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
                 <td><a href="meals?action=update&id=${meal.id}">Update</a></td>
-                <td><a href="meals?action=delete&id=${meal.id}">Delete</a></td>
+                <td><a href="meals/delete/${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>
